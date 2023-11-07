6d6f498eca4a666435fe23920055bd468b9cb796
AliasgerKing1 <aliasgersub34@gmail,com>
2023-11-06 23:50:35 -0800

    changes

---
+++ app_mod_backend/models/ModApp.js
@@ -0,0 +1,24 @@
+require("../config/database")
+const mongoose = require("mongoose");
+
+const ModApp = mongoose.Schema({
+    app_name: String,
+    upload_date: String,
+    mod_version : String,
+    category: String,
+    os : String,
+    isUpdatedToCloud : {
+        type : Boolean,
+        default : false
+    },
+    share_link : String,
+    description : {
+        type : String,
+        default : ""
+    },
+    cloudName : {
+        type : String,
+        default : ""
+    },
+})
+module.exports = mongoose.model("mod_app", ModApp);
